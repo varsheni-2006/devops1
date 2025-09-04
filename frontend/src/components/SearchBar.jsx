faf90740097ba3d5777787b61715e0bb25b313f0
varsheni-2006 <rajavarsheni@gmail.com>
2025-09-04 15:08:57 +0530

    Update SearchBar.jsx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+
+function SearchBar({ setSearch }) {
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search by blood group or location..."
+      onChange={(e) => setSearch(e.target.value)}
+    />
+  );
+}
+
+export default SearchBar;
 
